16a867e2c8d5579dfd4c8a8ababe15b7e93f1b09
Ricardo Santos <ri.santos@hotmail.com>
2018-12-06 10:03:29 -0200

    commit 1

---
+++ CoreStd/CoreStd.Model/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreStd.Model
+{
+    public class Class1
+    {
+    }
+}
